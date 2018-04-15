d53fa5ad17d82f81149d302a95be6d4b0ddafc1c
Jerome White <jerome@thewex.net>
2018-04-15 16:29:00 +0400

    Use Pandas/Matplotlib instead of Seaborn.
    Seaborn's point plot is not ideal; tsplot is
    preferable. Unfortunately, it's deprecated.

--- chapters/02/fig-2.1.py
+++ chapters/02/fig-2.1.py
@@ -1,16 +1,16 @@
 import sys
 
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(sys.stdin, index_col=False)
-df = df.melt(id_vars=['epsilon', 'bandit', 'play'],
-             value_vars=['reward', 'optimal'])
+df = (pd
+      .read_csv(sys.stdin, index_col=False)
+      .groupby(['epsilon', 'play'])
+      .mean()
+      .unstack(level=0))
 
-g = sns.FacetGrid(data=df, row='variable', hue='epsilon', sharey=False)
-g.map(sns.pointplot,
-      'play',
-      'value',
-      order=sorted(df['play'].unique()))
-g.savefig('2.1.png', bbox_inches='tight')
+plt.style.use('ggplot')
+(figure, axes) = plt.subplots(nrows=2, sharex=False)
+for (ax, factor) in zip(axes, ('reward', 'optimal')):
+    df[factor].plot(ax=ax)
+plt.savefig('2.1.png', bbox_inches='tight')
