3dda6609f654fb7d0dab10a5d7b124d706893e5d
Jerome White <jerome@thewex.net>
2018-04-15 16:45:08 +0400

    Convenience script to run exercises.

---
+++ chapters/02/exercise-2.2b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+epsilon=(
+    0
+    0.01
+    0.1
+)
+
+python exercise-2.2b.py \
+       --epsilon `sed -e's/ / --epsilon /g' <<< ${epsilon[@]}` \
+       --bandits 2000 \
+       --arms 10 \
+       --pulls 1000 |
+    python figure-2.1.py --output figure-2.1.png
