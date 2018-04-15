5325853450b75e9cf81dc9f162699b57e78bb351
Jerome White <jerome@thewex.net>
2018-04-15 16:34:28 +0400

    Separation of bandit logic from exercise execution.

--- chapters/02/bandit.py
+++ chapters/02/bandit.py
@@ -1,12 +1,6 @@
-import sys
-import csv
 import numpy as np
-import logging
 import operator as op
-import itertools as it
 import collections as cl
-import multiprocessing as mp
-from argparse import ArgumentParser
 
 Result = cl.namedtuple('Result', 'epsilon, bandit, play, reward, optimal')
 
@@ -53,48 +47,3 @@ class Bandit:
 
     def isoptimal(self, action):
         return action == max(self.actions, key=op.attrgetter('reward'))
-
-def run(incoming, outgoing, arms, pulls):
-    while True:
-        ((epsilon, bandit)) = incoming.get()
-
-        logging.info('{0} {1}'.format(epsilon, bandit))
-
-        b = Bandit(arms, epsilon)
-        for (play, action) in enumerate(it.islice(b, 0, pulls)):
-            b.do(action)
-            optimal = int(b.isoptimal(action))
-            outgoing.put(Result(epsilon, bandit, play, action.reward, optimal))
-        outgoing.put(None)
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='[ %(asctime)s ] %(levelname)s: %(message)s')
-
-arguments = ArgumentParser()
-arguments.add_argument('--bandits', type=int)
-arguments.add_argument('--arms', type=int)
-arguments.add_argument('--pulls', type=int)
-arguments.add_argument('--epsilon', action='append')
-arguments.add_argument('--workers', type=int, default=mp.cpu_count())
-args = arguments.parse_args()
-
-incoming = mp.Queue()
-outgoing = mp.Queue()
-
-initargs = (outgoing, incoming, args.arms, args.pulls)
-
-with mp.Pool(args.workers, run, initargs) as pool:
-    jobs = 0
-    for i in map(float, args.epsilon):
-        for j in range(args.bandits):
-            outgoing.put((i, j))
-            jobs += 1
-
-    writer = csv.DictWriter(sys.stdout, fieldnames=Result._fields)
-    writer.writeheader()
-    while jobs:
-        play = incoming.get()
-        if play is None:
-            jobs -= 1
-        else:
-            writer.writerow(play._asdict())
