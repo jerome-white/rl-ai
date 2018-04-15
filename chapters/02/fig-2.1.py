0f759c675566936a6cfda34bbda0a0c1be7d9a56
Jerome White <jerome@thewex.net>
2018-04-15 16:34:12 +0400

    Ability to specify output via command line.

--- chapters/02/fig-2.1.py
+++ chapters/02/fig-2.1.py
@@ -1,7 +1,12 @@
 import sys
+from argparse import ArgumentParser
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+arguments = ArgumentParser()
+arguments.add_argument('--output', type=Path)
+args = arguments.parse_args()
 
 df = (pd
       .read_csv(sys.stdin, index_col=False)
@@ -13,4 +18,4 @@ plt.style.use('ggplot')
 (figure, axes) = plt.subplots(nrows=2, sharex=False)
 for (ax, factor) in zip(axes, ('reward', 'optimal')):
     df[factor].plot(ax=ax)
-plt.savefig('2.1.png', bbox_inches='tight')
+plt.savefig(str(args.output), bbox_inches='tight')
