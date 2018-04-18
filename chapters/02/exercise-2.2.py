55a17fdb9bed98ef83f7822c574f9c7283fdce05
Jerome White <jerome@thewex.net>
2018-04-18 10:03:00 +0400

    Temperature support.

--- chapters/02/exercise-2.2.py
+++ chapters/02/exercise-2.2.py
@@ -1,23 +1,31 @@
 import sys
 import csv
 import logging
+import inspect
 import itertools as it
+import collections as cl
 import multiprocessing as mp
 from argparse import ArgumentParser
 
-from bandit import Bandit, Result
+from bandit import Bandit
 
-def run(incoming, outgoing, arms, pulls):
+bsig = inspect.signature(Bandit)
+BanditArgs = cl.namedtuple('BanditArgs', bsig.parameters.keys())
+fields = BanditArgs._fields + ('bandit', 'play', 'reward', 'optimal')
+Result = cl.namedtuple('Result', fields)
+
+def run(incoming, outgoing, pulls):
     while True:
-        ((epsilon, bandit)) = incoming.get()
+        (bid, bargs) = incoming.get()
+        logging.info('{0}: {1}'.format(bid, bargs))
 
-        logging.info('{0} {1}'.format(epsilon, bandit))
-
-        b = Bandit(arms, epsilon)
+        b = Bandit(*bargs)
         for (play, action) in enumerate(it.islice(b, 0, pulls)):
             reward = b.pull(action)
             optimal = int(b.isoptimal(action))
-            outgoing.put(Result(epsilon, bandit, play, reward, optimal))
+            result = it.chain(bargs, (bid, play, reward, optimal))
+
+            outgoing.put(Result._make(result))
         outgoing.put(None)
 
 logging.basicConfig(level=logging.DEBUG,
@@ -27,24 +35,30 @@ arguments = ArgumentParser()
 arguments.add_argument('--bandits', type=int)
 arguments.add_argument('--arms', type=int)
 arguments.add_argument('--pulls', type=int)
-arguments.add_argument('--epsilon', action='append')
+arguments.add_argument('--epsilon', type=float, action='append')
+arguments.add_argument('--temperature', type=float, action='append')
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
 incoming = mp.Queue()
 outgoing = mp.Queue()
 
-initargs = (outgoing, incoming, args.arms, args.pulls)
+initargs = (outgoing, incoming, args.pulls)
 
 with mp.Pool(args.workers, run, initargs) as pool:
     jobs = 0
-    for i in map(float, args.epsilon):
-        for j in range(args.bandits):
-            outgoing.put((i, j))
-            jobs += 1
+    temperature = [ 0 ] if args.temperature is None else args.temperature
+
+    for (i, *args) in it.product(range(args.bandits),
+                                [ args.arms ],
+                                args.epsilon,
+                                temperature):
+        outgoing.put((i, BanditArgs._make(args)))
+        jobs += 1
 
     writer = csv.DictWriter(sys.stdout, fieldnames=Result._fields)
     writer.writeheader()
+
     while jobs:
         play = incoming.get()
         if play is None:
