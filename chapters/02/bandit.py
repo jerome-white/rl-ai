dbfd8a34ef861247d270a553e87bdb1d9b34667f
Jerome White <jerome@thewex.net>
2018-04-21 13:40:18 +0400

    Fold averaging into bandit.
    The weighted average is essentially a generality of the sample.

--- chapters/02/bandit.py
+++ chapters/02/bandit.py
@@ -3,64 +3,43 @@ import collections as cl
 import pandas as pd
 import numpy as np
 
-class Average:
-    def __init__(self):
-        self.k = 0
-        self.value = float(0)
-
-    def __float__(self):
-        return self.value
-
-    def update(self, value):
-        raise NotImplementedError()
-
-class SampleAverage(Average):
-    def __init__(self):
-        super().__init__()
-
-    def update(self, value):
-        k = self.k + 1
-        self.value = (self.k * self.value + value) / k
-        self.k = k
-
-class NonStationaryAverage(Average):
-    def __init__(self, alpha=None):
-        super().__init__()
-        self.step = alpha
-
-    def alpha(self):
-        return 1 / (self.k + 1) if self.step is None else self.step
-
-    def update(self, value):
-        self.value += self.alpha() * (value - self.value)
-        self.k += 1
-
 class Action:
-    def __init__(self, name):
+    def __init__(self, name, step=None):
         self.name = name
+        self.step = step
+
         self.reward = np.random.normal()
-        self.estimate = SampleAverage()
+        self.pulls = 0
+        self.estimate = 0
 
     def __eq__(self, other):
         return self.name == other.name
 
+    def __float__(self):
+        return self.estimate
+
+    # def __gt__(self, other):
+    #     return float(self) > float(other)
+
     def execute(self):
         reward = np.random.normal(self.reward)
-        self.estimate.update(reward)
+
+        alpha = 1 / (self.pulls + 1) if self.step is None else self.alpha
+        self.estimate += alpha * (value - self.estimate)
+        self.pulls += 1
 
         return reward
 
 class Bandit:
     def __init__(self, arms, epsilon=0, temperature=0):
-        self.arms = arms
         self.epsilon = epsilon
 
         if temperature:
-            self.softmax = lambda x: np.exp(float(x.estimate) / temperature)
+            self.softmax = lambda x: np.exp(float(x) / temperature)
         else:
             self.softmax = None
 
-        self.actions = [ Action(x) for x in range(self.arms) ]
+        self.actions = [ Action(x) for x in range(arms) ]
 
     def __iter__(self):
         return self
@@ -76,7 +55,7 @@ class Bandit:
             action = np.random.choice(self.actions, p=p)
         else:
             # exploit
-            estimates = [ float(x.estimate) for x in self.actions ]
+            estimates = list(map(float, self.actions))
             largest = np.argwhere(estimates == np.max(estimates)).flatten()
             action = self.actions[np.random.choice(largest)]
 
