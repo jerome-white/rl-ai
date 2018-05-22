76870ed97beb5b7e0b0cd57750f7a526626ad00a
Jerome White <jerome@thewex.net>
2018-05-22 10:20:27 +0400

    Iteration of actions.
    Right now, only for a single location.

--- chapters/04/exercise-4.4.py
+++ chapters/04/exercise-4.4.py
@@ -1,45 +1,79 @@
+import math
 import itertools as it
+import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
 from configparser import ConfigParser
 
-class States:
-    def __init__(self, cars, locations):
+Location = cl.namedtuple('Location', 'cars, requests, returns')
+State = cl.namedtuple('State', 'first, second')
+Action = cl.namedtuple('Action', 'prob, reward, state')
+
+class System:
+    def __init__(self, cars, moves, locations):
         self.cars = cars
+        self.moves = moves
         self.locations = locations
 
     def __iter__(self):
-        cars = range(self.cars)
-        yield from it.combinations_with_replacement(cars, self.locations)
+        yield from it.product(irange(self.cars),
+                              irange(self.cars),
+                              irange(self.moves))
+#                               repeat=self.locations)
 
-class Actions:
-    def __init__(self, locations):
-        self.locations = locations
+def irange(stop):
+    yield from range(int(stop) + 1)
 
-    def __iter__(self):
-        for rented in range(20):
-            reward = rented * self.credit
-            for returned in range(20 - rented):
-                for moved in range(5):
-                    reward - moved * self.cost
+def poisson(lam, n):
+    return lam ** n / math.factorial(n) * math.e ** -lam
+
+def states(system, locations):
+    for cars in it.combinations_with_replacement(irange(system.cars),
+                                                 len(locations)):
+        locs = []
+        for (i, j) in zip(cars, locations):
+            locs.append(Location(i, j['requests'], j['returns']))
+
+        yield State(*locs)
+
+def actions(state, system, costs):
+    for (rented, returned, moved) in system:
+        net = state.first.cars - rented + returned - moved
+        if 0 <= net <= system.cars:
+            prob = poisson(state.first.requests, rented)
+            prob *= poisson(state.first.returns, returned)
+            reward = rented * costs['rental'] + moved * costs['move']
+
+            yield Action(prob, reward, None)
 
 arguments = ArgumentParser()
 arguments.add_argument('--config', type=Path)
+arguments.add_argument('--discount', type=float)
 args = arguments.parse_args()
 
 config = ConfigParser()
-config.read(str(args.config))
+config.read(args.config)
 
-states = States(int(config['capacity']), len(config) - 1)
+env = cl.defaultdict(dict)
+for (i, j) in config.items():
+    for (k, v) in j.items():
+        env[i][k] = float(v)
 
-while True:
-    # policy evaluation
-    while True:
-        delta = 0
-        for s in states:
-            pass
+syscfg = env['system']
+system = System(syscfg['capacity'], syscfg['movable'], 2)
 
-    # policy improvement
-    stable = True
-    for s in states:
-        b = pi(s)
+for s in states(system, (env['first'], env['second'])):
+    for a in actions(s, system, env['cost']):
+        print(a)
+
+# while True:
+#     # policy evaluation
+#     while True:
+#         delta = 0
+#         for s in states:
+#             pass
+
+#     # policy improvement
+#     stable = True
+#     for s in states:
+#         b = pi(s)
