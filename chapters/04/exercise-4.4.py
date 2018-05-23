f4b74db97de349b56fbe576edc2c26d0d587a07d
Jerome White <jerome@thewex.net>
2018-05-23 17:02:48 +0400

    Rework of location car possiblities.
    Consider the perspective of the location manager at the end of the
    day: there are so many cars on the lot, what are all of the potential
    ways in which this happend? In some cases, cars got moved from the
    other location -- permutations at the other location should take this
    into account.

--- chapters/04/exercise-4.4.py
+++ chapters/04/exercise-4.4.py
@@ -1,70 +1,89 @@
 import math
+import logging
 import itertools as it
 import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
 from configparser import ConfigParser
 
-Location = cl.namedtuple('Location', 'cars, requests, returns')
+logging.basicConfig(level=logging.INFO,
+                    format='[ %(asctime)s ] %(levelname)s: %(message)s',
+                    datefmt='%H:%M:%S')
+
 State = cl.namedtuple('State', 'first, second')
 Action = cl.namedtuple('Action', 'prob, reward, state')
+Dynamic = cl.namedtuple('Dynamic', 'rented, returned, moved')
 
-class System:
-    def __init__(self, cars, moves, locations):
-        self.cars = cars
-        self.moves = moves
-        self.locations = locations
+class Location:
+    def __init__(self, rentals, returns):
+        self.rentals = rentals
+        self.returns = returns
 
-    def __iter__(self):
-        yield from it.product(irange(self.cars),
-                              irange(self.cars),
-                              irange(self.moves))
-#                               repeat=self.locations)
+    def prob(self, dynamic):
+        rentals = Location.poisson(self.rentals, abs(dynamic.rented))
+        returned = Location.poisson(self.returns, dynamic.returned)
+
+        return rentals * returned
+
+    @staticmethod
+    def poisson(lam, n):
+        return lam ** n / math.factorial(n) * math.e ** -lam
 
 def irange(stop):
-    yield from range(int(stop) + 1)
+    yield from range(stop + 1)
 
-def poisson(lam, n):
-    return lam ** n / math.factorial(n) * math.e ** -lam
+def states(capacity):
+    yield from it.starmap(State, it.product(irange(capacity), repeat=2))
 
-def states(system, locations):
-    for cars in it.combinations_with_replacement(irange(system.cars),
-                                                 len(locations)):
-        locs = []
-        for (i, j) in zip(cars, locations):
-            locs.append(Location(i, j['requests'], j['returns']))
+def actions(cars, capacity, movable):
+    try:
+        iter(movable)
+    except TypeError:
+        movable = (movable, )
 
-        yield State(*locs)
+    rentable = range(-capacity, 1)
+    returnable = irange(capacity)
 
-def actions(state, system, costs):
-    for (rented, returned, moved) in system:
-        net = state.first.cars - rented + returned - moved
-        if 0 <= net <= system.cars:
-            prob = poisson(state.first.requests, rented)
-            prob *= poisson(state.first.returns, returned)
-            reward = rented * costs['rental'] + moved * costs['move']
+    for i in it.starmap(Dynamic, it.product(rentable, returnable, movable)):
+        if capacity - sum(i) == cars:
+            yield i
 
-            yield Action(prob, reward, None)
+def evaluate(state, first, second):
+    for i in actions(state.first, 20, irange(5)):
+        first_ = state.first + i.moved
+        partial_reward = 10 * abs(i.rented) - 2 * i.moved
 
-arguments = ArgumentParser()
-arguments.add_argument('--config', type=Path)
-arguments.add_argument('--discount', type=float)
-args = arguments.parse_args()
+        for j in actions(state.second, 20, -i.moved):
+            logging.debug('{0} {1}'.format(state.first, i))
+            logging.debug('{0} {1}'.format(state.second, j))
 
-config = ConfigParser()
-config.read(args.config)
+            prob = first.prob(i) * second.prob(j)
+            reward = partial_reward + 10 * abs(j.rented)
+            state_ = State(first_, state.second - i.moved)
 
-env = cl.defaultdict(dict)
-for (i, j) in config.items():
-    for (k, v) in j.items():
-        env[i][k] = float(v)
+            yield Action(prob, reward, state_)
 
-syscfg = env['system']
-system = System(syscfg['capacity'], syscfg['movable'], 2)
+first = Location(3, 3)
+second = Location(4, 2)
+for s in states(20):
+    for e in evaluate(s, first, second):
+        print(e)
 
-for s in states(system, (env['first'], env['second'])):
-    for a in actions(s, system, env['cost']):
-        print(a)
+# arguments = ArgumentParser()
+# arguments.add_argument('--config', type=Path)
+# arguments.add_argument('--discount', type=float)
+# args = arguments.parse_args()
+
+# config = ConfigParser()
+# config.read(args.config)
+
+# env = cl.defaultdict(dict)
+# for (i, j) in config.items():
+#     for (k, v) in j.items():
+#         env[i][k] = float(v)
+
+# syscfg = env['system']
+# system = System(syscfg['capacity'], syscfg['movable'], 2)
 
 # while True:
 #     # policy evaluation
