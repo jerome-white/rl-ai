f91c2f44cabf6a99218785e0b780a54dc7acd8f2
Jerome White <jerome@thewex.net>
2018-05-24 11:09:01 +0400

    Integration of framework into policy iteration.

--- chapters/04/exercise-4.4.py
+++ chapters/04/exercise-4.4.py
@@ -51,6 +51,18 @@ class Policy:
 
                 yield Action(prob, reward, state_)
 
+class Bellman:
+    def __init__(self, policy, discount, first, second):
+        self.policy = policy
+        self.discount = discount
+        self.first = first
+        self.second = second
+
+    def evaluate(state, estimate):
+        for e in self.policy.evaluate(state, self.first, self.second):
+            existing = estimate[e.state.first][e.state.second]
+            yield e.prob * (e.reward + self.discount * existing)
+
 def irange(stop):
     yield from range(stop + 1)
 
@@ -75,6 +87,7 @@ def actions(cars, capacity, movable):
 arguments = ArgumentParser()
 arguments.add_argument('--config', type=Path)
 arguments.add_argument('--discount', type=float)
+arguments.add_argument('--improvement-threshold', type=float)
 args = arguments.parse_args()
 
 config = ConfigParser()
@@ -105,14 +118,24 @@ for s in states(capacity, len(locations)):
     for e in policy.evaluate(s, *locations):
         print(e)
 
-# while True:
-#     # policy evaluation
-#     while True:
-#         delta = 0
-#         for s in states:
-#             pass
+bellman = Bellman(policy, args.discount, *locations)
+backup = np.zeros()
 
-#     # policy improvement
-#     stable = True
-#     for s in states:
-#         b = pi(s)
+while True:
+    #
+    # policy evaluation
+    #
+    delta = None
+
+    while delta is None or delta > args.improvement_threshold:
+        backup_ = np.zeros()
+        for s in states:
+            rewards = bellman.evaluate(s, backup)
+            backup_[s.first][s.second] = sum(rewards)
+        delta = np.sum(np.abs(backup_ - backup))
+        backup = backup_
+
+    # policy improvement
+    stable = True
+    for s in states:
+        b = pi(s)
