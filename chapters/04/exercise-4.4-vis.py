e81a37ba931a3a0ff9959357440e8cf285f812a9
Jerome White <jerome@thewex.net>
2018-05-28 14:31:08 +0400

    Use class to manage data iteration.
    For some reason using the procedural approach lost the first frame.

--- chapters/04/exercise-4.4-vis.py
+++ chapters/04/exercise-4.4-vis.py
@@ -5,27 +5,52 @@ from configparser import ConfigParser
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import constants
 from matplotlib.animation import FuncAnimation
 
-def func(frame, limit):
-    (i, (_, data)) = frame
+class Animator:
+    def __init__(self, npz, limit=None):
+        self.data = []
+        self.ax = None
 
-    plt.clf()
-    ax = sns.heatmap(data, vmin=-limit, vmax=limit)
-    ax.set_title('Iteration: {}'.format(i))
-    ax.invert_yaxis()
+        if limit is None:
+            self.vmin = None
+            self.vmax = None
+        else:
+            (self.vmin, self.vmax) = (-limit, limit)
 
-    return [ ax ]
-        
+        load = np.load(npz)
+        for (_, data) in load.items():
+            if limit is None:
+                (x, y) = [ f(data) for f in (np.min, np.max) ]
+                self.vmin = x if self.vmin is None else min(self.vmin, x)
+                self.vmax = y if self.vmax is None else max(self.vmax, y)
+
+            self.data.append(data)
+
+    def __iter__(self):
+        yield from enumerate(self.data)
+
+    def func(self, frame):
+        (i, data) = frame
+
+        plt.clf()
+        ax = sns.heatmap(data, vmin=self.vmin, vmax=self.vmax)
+        ax.set_title('Iteration: {}'.format(i))
+        ax.invert_yaxis()
+
+        return (ax, )
+
 arguments = ArgumentParser()
 arguments.add_argument('--data', type=Path)
 arguments.add_argument('--limit', type=int)
+arguments.add_argument('--delay', type=int, default=2)
 args = arguments.parse_args()
 
-data = np.load(args.data)
+interval = args.delay / constants.milli
+animator = Animator(args.data, args.limit)
 ani = FuncAnimation(plt.gcf(),
-                    func,
-                    frames=enumerate(data.items()),
-                    fargs=(args.limit, ),
-                    interval=1000)
+                    animator.func,
+                    frames=animator,
+                    interval=interval)
 ani.save(args.data.with_suffix('.mp4'))
