04c19cdd5f50d19713c683b553262ba578acfc4a
Jerome White <jerome@thewex.net>
2018-06-19 15:23:19 +0400

    Example 5.1 (Monte Carlo ES).

---
+++ chapters/05/example-5.3.py
@@ -0,0 +1,49 @@
+import logging
+import collections as cl
+from argparse import ArgumentParser
+
+import numpy as np
+
+from blackjack import State, Blackjack
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[ %(asctime)s ] %(levelname)s: %(message)s',
+                    datefmt='%H:%M:%S')
+
+def states():
+    while True:
+        args = (range(12, 21 + 1), range(1, 10 + 1), (True, False))
+        yield State(*map(random.choice, args))
+
+arguments = ArgumentParser()
+arguments.add_argument('--games', type=int)
+arguments.add_argument('--with-ace', action='store_true')
+args = arguments.parse_args()
+
+Q = cl.defaultdict(lambda: [ -np.inf ] * len(('hit', 'stick')))
+policy = {}
+
+for state in it.takewhile(lambda _: args.games > 0, states()):
+    #
+    # generate epsiode
+    #
+    blackjack = Blackjack(state)
+    (episode, reward) = blackjack.play()
+
+    logging.info('{0}: {1} {2}'.format(args.games, state, reward))
+
+    #
+    # calculate returns
+    #
+    for (state, action) in episode:
+        Q[state][action].append(reward)
+
+    for (state, _) in episode:
+        policy[state] = np.argmax(Q[state])
+
+    args.games -= 1
+
+V = np.zeros((21 - 12 + 1, 10 - 2 + 1))
+for (k, v) in values.items():
+    if args.with_ace ^ v.ace:
+        V[k.player, k.dealer] = float(v)
