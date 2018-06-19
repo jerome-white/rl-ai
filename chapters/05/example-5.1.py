3b0d7826226f6e1b8cf7283ce6a8eb37ed46e562
Jerome White <jerome@thewex.net>
2018-06-19 15:23:06 +0400

    Uses blackjack module.

--- chapters/05/example-5.1.py
+++ chapters/05/example-5.1.py
@@ -1,139 +1,38 @@
-import random
 import logging
-import itertools as it
 import collections as cl
 from argparse import ArgumentParser
+
+import numpy as np
+
+from blackjack import Blackjack
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[ %(asctime)s ] %(levelname)s: %(message)s',
                     datefmt='%H:%M:%S')
-
-Card = cl.namedtuple('Card', 'suit, value')
-State = cl.namedtuple('State', 'player, dealer, ace')
-
-class Average:
-    def __init__(self):
-        self.average = 0
-        self.n = 1
-
-    def add(self, value):
-        self.average += (value - self.average) / self.n
-        self.n += 1
-
-    def __float__(self):
-        return self.average
-
-class Deck(list):
-    def __init__(self):
-        for i in range(4):
-            for j in range(13):
-                value = min(j + 1, 10) if j else None
-                self.append(Card(i, value))
-
-    def __next__(self):
-        return random.choice(self)
-
-class Policy:
-    def __init__(self):
-        self.ace = False
-        self.cards = []
-        self.value = 0
-
-    def __int__(self):
-        return self.value
-
-    def __bool__(self):
-        return int(self) <= 21 and not self.stick()
-
-    def __str__(self):
-        return '[{0}]'.format(','.join([ str(x.value) for x in self.cards ]))
-
-    def deal(self, card):
-        self.cards.append(card)
-
-        try:
-            self.value += card.value
-        except TypeError:
-            if self.value + 11 <= 21:
-                self.value += 11
-                self.ace = True
-            else:
-                self.value += 1
-
-        if self.value > 21:
-            raise OverflowError()
-
-    def isnatural(self):
-        return len(self.cards) == 2 and int(self) == 21
-
-    def stick(self):
-        raise NotImplementedError()
-
-class Dealer(Policy):
-    def stick(self):
-        return int(self) >= 17
-
-class Player(Policy):
-    def stick(self):
-        return 20 <= int(self) <= 21
-
-def play():
-    episode = []
-
-    deck = Deck()
-    dealer = Dealer()
-    player = Player()
-
-    for p in (player, dealer):
-        for _ in range(2):
-            p.deal(next(deck))
-
-    face = dealer.cards[0].value
-    episode = [ State(int(player), face, player.ace) ]
-
-    if player.isnatural():
-        return (episode, int(not dealer.isnatural()))
-
-    for (i, p) in enumerate((player, dealer)):
-        while p:
-            bust = False
-            try:
-                p.deal(next(deck))
-            except OverflowError:
-                bust = True
-
-            if not i:
-                episode.append(State(int(p), face, p.ace))
-
-            if bust:
-                reward = 1 if i else -1
-                return (episode, reward)
-
-    (p, d) = [ int(x) for x in (player, dealer) ]
-    reward = ((p > d) - (p < d)) # https://stackoverflow.com/a/11215908
-
-    return (episode, reward)
 
 arguments = ArgumentParser()
 arguments.add_argument('--games', type=int)
 arguments.add_argument('--with-ace', action='store_true')
 args = arguments.parse_args()
 
-values = cl.defaultdict(Average)
-for _ in range(args.games):
+returns = cl.defaultdict(list)
+
+for i in range(args.games):
     #
     # generate epsiode
     #
-    (episode, reward) = play()
-    logging.info('{0} {1}'.format(','.join(map(str, episode)), reward))
+    blackjack = Blackjack()
+    (episode, reward) = blackjack.play()
+
+    logging.info('{0}: {1}'.format(i, reward))
 
     #
     # calculate returns
     #
-    for state in episode:
-        values[state].add(reward)
+    for (state, _) in episode:
+        returns[state].append(reward)
 
 V = np.zeros((21 - 12 + 1, 10 - 2 + 1))
 for (k, v) in values.items():
-    if args.with_ace ^ v.ace:
-        V[k.player, k.dealer] = float(v)
+    if args.with_ace ^ k.ace:
+        V[k.player, k.dealer] = np.mean(v)
