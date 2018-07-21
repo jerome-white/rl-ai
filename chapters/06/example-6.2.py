c54df0af4ff2076a284ff4b064d9a70bc6bcbf8e
Jerome White <jerome@thewex.net>
2018-07-21 12:44:40 +0400

    Flesh out functionality.
    Complete logging of each state. Final values are still not correct.

--- chapters/06/example-6.2.py
+++ chapters/06/example-6.2.py
@@ -1,18 +1,40 @@
+import sys
+import csv
+import logging
 import itertools as it
+import collections as cl
 from argparse import ArgumentParser
 
 import walk
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
+
 arguments = ArgumentParser()
-arguments.add_argument('--states', type=int, default=4)
+arguments.add_argument('--states', type=int, default=5)
+arguments.add_argument('--episodes', type=int, default=1)
+arguments.add_argument('--gamma', type=float, default=1)
+arguments.add_argument('--alpha', type=float, default=1)
 args = arguments.parse_args()
 
-states = args.states + 2
-V = np.zeros(states + 1)
+states = args.states + 1
+V = cl.defaultdict(lambda: 0.5)
 
-s = None
-for s_ in walk.walk(states):
-    if s is not None:
-        value = (s.reward + args.gamma * V[s_.state] - V[s.state])
-        V[s.state] += args.alpha * value
-    s = s_
+fieldnames = [ chr(65 + x) for x in range(args.states) ]
+writer = csv.DictWriter(sys.stdout,
+                        fieldnames=fieldnames,
+                        restval=0.5,
+                        extrasaction='ignore')
+writer.writeheader()
+writer.writerow(V)
+
+for i in range(args.episodes):
+    s = None
+    for s_ in walk.walk(states):
+        if s is not None:
+            value = s.reward + args.gamma * V[s_.state] - V[s.state]
+            V[s.state] += args.alpha * value
+        s = s_
+
+    writer.writerow(V)
