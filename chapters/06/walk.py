8c63ed8069ab521299ebd1623201c73c483d8eb3
Jerome White <jerome@thewex.net>
2018-07-21 12:44:14 +0400

    Use letters as state names.

--- chapters/06/walk.py
+++ chapters/06/walk.py
@@ -7,11 +7,15 @@ def walk(states=6, start=None):
     if start is None:
         start = states // 2
 
-    while 0 < start < states:
+    while True:
         action = random.choice((-1, 1))
         start_ = start + action
         reward = 1 if start_ == states else 0
 
-        yield Transition(start, action, reward)
+        state = chr(ord('A') + start - 1)
 
+        yield Transition(state, action, reward)
+
+        if not (0 < start < states):
+            break
         start = start_
