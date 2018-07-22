7abb499bf3166ae7e8ddefa233218cd0a642997f
Jerome White <jerome@thewex.net>
2018-07-22 09:03:35 +0400

    States provided as actual states (instead of count).

--- chapters/06/walk.py
+++ chapters/06/walk.py
@@ -1,21 +1,26 @@
 import random
+import operator as op
 import collections as cl
+
+import numpy as np
 
 Transition = cl.namedtuple('Transition', 'state, action, reward')
 
-def walk(states=6, start=None):
-    if start is None:
-        start = states // 2
+def walk(states, pos=None, terminal=np.inf):
+    assert(not any([ f(terminal) in states for f in (op.neg, op.pos) ]))
 
-    while True:
+    n = len(states)
+
+    if pos is None:
+        pos = n // 2
+
+    while 0 <= pos < n:
         action = random.choice((-1, 1))
-        start_ = start + action
-        reward = 1 if start_ == states else 0
+        pos_ = pos + action
+        reward = 0 if pos_ < n else 1
 
-        state = chr(ord('A') + start - 1)
+        yield Transition(states[pos], action, reward)
 
-        yield Transition(state, action, reward)
+        pos = pos_
 
-        if not (0 < start < states):
-            break
-        start = start_
+    yield Transition(np.sign(pos) * terminal, 0, np.nan)
