704c886d675e621623cb24882f0d9f2cffada71c
Jerome White <jerome@thewex.net>
2018-07-24 14:10:48 +0400

    Averages episodes over runs.
    Book implies that each 100-length episode is run and averaged 100
    times. This version does that in parallel.
    
    Also uses Seaborn to plot the data. The most up-to-date version of the
    library supports a lineplot, but that version is not yet available in
    Anaconda so lmplot is used.

--- chapters/06/example-6.2b.py
+++ chapters/06/example-6.2b.py
@@ -1,12 +1,13 @@
+import math
 import logging
 import itertools as it
-import collections as cl
 import multiprocessing as mp
 from argparse import ArgumentParser
 
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import sklearn.metrics as skm
-import matplotlib.pyplot as plt
 
 from walk import TemporalDifference, MonteCarlo
 
@@ -14,9 +15,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S')
 
-System = cl.namedtuple('System', 'model, alpha')
-
-def func(incoming, outgoing, states, episodes):
+def func(incoming, outgoing, states, episodes, runs):
     exclusion = states + 1
     actual = [ x / exclusion for x in range(1, exclusion) ]
 
@@ -26,43 +25,59 @@ def func(incoming, outgoing, states, episodes):
     }
 
     while True:
-        system = incoming.get()
-        logging.debug(system)
+        (method, alpha) = incoming.get()
 
-        rmse = []
-        m = Model[system.model](states, episodes, system.alpha)
+        for i in range(runs):
+            logging.info('{0} {1} {2}'.format(method, alpha, i))
 
-        for i in m:
-            predicted = [ i[x] for x in m.states ]
-            mse = skm.mean_squared_error(actual, predicted)
-            rmse.append(mse)
+            model = Model[method](states, episodes, alpha)
+            for (j, m) in enumerate(model):
+                predicted = [ m[x] for x in model.states ]
+                rmse = math.sqrt(skm.mean_squared_error(actual, predicted))
 
-        outgoing.put((system, rmse))
+                outgoing.put({
+                    'model': '{0}:{1}'.format(method, alpha),
+                    'run': i,
+                    'step': j,
+                    'RMSE': rmse,
+                })
+
+        outgoing.put(None)
+
+def do(args):
+    incoming = mp.Queue()
+    outgoing = mp.Queue()
+
+    initargs = (outgoing, incoming, args.states, args.episodes, args.runs)
+
+    with mp.Pool(args.workers, func, initargs):
+        td_alpha = np.linspace(0.05, 0.15, 3)
+        mc_alpha = np.linspace(0.01, 0.04, 4)
+
+        for (model, alpha) in zip(('td', 'mc'), (td_alpha, mc_alpha)):
+            for a in alpha:
+                outgoing.put((model, a))
+
+        jobs = len(td_alpha) + len(mc_alpha)
+        while jobs:
+            value = incoming.get()
+            if value is None:
+                jobs -= 1
+            else:
+                yield value
 
 arguments = ArgumentParser()
 arguments.add_argument('--states', type=int, default=5)
+arguments.add_argument('--runs', type=int, default=100)
 arguments.add_argument('--episodes', type=int, default=100)
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
-incoming = mp.Queue()
-outgoing = mp.Queue()
+df = pd.DataFrame(do(args))
 
-initargs = (outgoing, incoming, args.states, args.episodes)
-
-with mp.Pool(args.workers, func, initargs):
-    td_alpha = np.linspace(0.05, 0.15, 3)
-    mc_alpha = np.linspace(0.01, 0.04, 4)
-
-    for (model, alpha) in zip(('td', 'mc'), (td_alpha, mc_alpha)):
-        for a in alpha:
-            system = System(model, a)
-            outgoing.put(system)
-
-    jobs = len(td_alpha) + len(mc_alpha)
-    for _ in range(jobs):
-        (system, rmse) = incoming.get()
-        plt.plot(rmse, label=str(system))
-    plt.legend()
-
-    plt.savefig('example-6.2b.png')
+ax = sns.pointplot(x='step',
+                   y='RMSE',
+                   hue='model',
+                   ci=None,
+                   data=df)
+ax.get_figure().savefig('example-6.2b.png')
