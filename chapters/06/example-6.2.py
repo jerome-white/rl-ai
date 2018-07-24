fd4cd268cd43c6644298c44380429d850b60f07c
Jerome White <jerome@thewex.net>
2018-07-24 09:08:39 +0400

    Example code only produces graphs.

--- chapters/06/example-6.2.py
+++ chapters/06/example-6.2.py
@@ -1,27 +1,26 @@
-import sys
-import csv
-import logging
-import itertools as it
-import collections as cl
 from argparse import ArgumentParser
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 from walk import TemporalDifference
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S')
-
 arguments = ArgumentParser()
 arguments.add_argument('--states', type=int, default=5)
-arguments.add_argument('--episodes', type=int, default=1)
-arguments.add_argument('--alpha', type=float, default=1)
-arguments.add_argument('--gamma', type=float, default=1)
+arguments.add_argument('--episodes', type=int, default=1000)
+arguments.add_argument('--alpha', type=float, default=0.1)
+arguments.add_argument('--samples', action='append')
 args = arguments.parse_args()
 
-model = TemporalDifference(args.states, args.episodes, args.alpha, args.gamma)
+if args.samples:
+    samples = args.samples
+else:
+    samples = set([0] + np.logspace(0, 3, 4, dtype=int).tolist())
 
-writer = csv.DictWriter(sys.stdout,
-                        fieldnames=model.states,
-                        extrasaction='ignore')
-writer.writeheader()
-writer.writerows(model)
+model = TemporalDifference(args.states, args.episodes, args.alpha)
+df = pd.DataFrame.from_dict(dict(enumerate(model)), orient='index')
+df = df[df.index.isin(samples)]
+
+ax = sns.lineplot(data=df)
+ax.get_figure().savefig('example-6.2.png')
