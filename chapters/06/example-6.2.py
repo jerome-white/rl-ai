e4348ef2ed0e1932f75c6214e8855ee3043c6f8e
Jerome White <jerome@thewex.net>
2018-07-24 09:47:08 +0400

    Builds an animation.
    Seaborn lineplot is very new, and not included in all Anaconda
    distributions yet. An animation is almost more effective at showing
    the dynamics of the system.

--- chapters/06/example-6.2.py
+++ chapters/06/example-6.2.py
@@ -1,26 +1,50 @@
+import logging
 from argparse import ArgumentParser
 
 import numpy as np
-import pandas as pd
-import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from walk import TemporalDifference
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S')
+
+def func(frame):
+    (i, data) = frame
+
+    logging.info(i)
+
+    states = sorted(data.keys())
+    y = [ data[x] for x in states ]
+    limit = len(states) + 1
+    samples = set([0] + np.logspace(0, 3, 4, dtype=int).tolist())
+
+    plt.clf()
+
+    plt.plot([ data[x] for x in states ], label='Estimate')
+    plt.plot([ x / limit for x in range(1, limit) ], label='True')
+
+    plt.grid(True)
+    plt.title('Step ' + str(i))
+    plt.xticks(range(len(states)), states)
+    plt.ylim(0, 1)
+    plt.legend(loc='upper left')
+
+    return (plt.gca(), )
 
 arguments = ArgumentParser()
 arguments.add_argument('--states', type=int, default=5)
 arguments.add_argument('--episodes', type=int, default=1000)
 arguments.add_argument('--alpha', type=float, default=0.1)
-arguments.add_argument('--samples', action='append')
 args = arguments.parse_args()
 
-if args.samples:
-    samples = args.samples
-else:
-    samples = set([0] + np.logspace(0, 3, 4, dtype=int).tolist())
-
 model = TemporalDifference(args.states, args.episodes, args.alpha)
-df = pd.DataFrame.from_dict(dict(enumerate(model)), orient='index')
-df = df[df.index.isin(samples)]
-
-ax = sns.lineplot(data=df)
-ax.get_figure().savefig('example-6.2.png')
+ani = FuncAnimation(plt.gcf(),
+                    func,
+                    frames=enumerate(model),
+                    init_func=lambda: (plt.gca(), ),
+                    interval=50,
+                    save_count=args.episodes)
+ani.save('example-6.2.mp4')
