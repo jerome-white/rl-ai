f01d8a61f6c916c35ff43c474865d978b5c0d759
Jerome White <jerome@thewex.net>
2018-12-23 17:16:42 +0530

    More like Example 6.5.
    This essentially Example 6.5 with different types of learning.

--- chapters/06/example-6.6.py
+++ chapters/06/example-6.6.py
@@ -1,10 +1,11 @@
-import sys
-import csv
 import logging
-import collections as cl
+import functools as ft
+import multiprocessing as mp
 from argparse import ArgumentParser
 
-import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import gridworld as gw
 from cliff import Cliff
@@ -13,40 +14,75 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S')
 
+class ClassicGrid(gw.GridWorld):
+    def __init__(self, shape, goal):
+        super().__init__(shape, goal, gw.FourPointCompass(), gw.Wind())
+
+def func(incoming, outgoing, args):
+    keys = [ 'order', 'experiment', 'episode', 'reward' ]
+
+    shape = (4, 12)
+    (row, column) = [ x - 1 for x in shape ]
+    start = gw.State(row, 0)
+    goal = gw.State(row, column)
+
+    while True:
+        (order, experiment, Learning) = incoming.get()
+
+        logging.info('{} {}'.format(order, experiment))
+
+        grid = Cliff(shape, goal, start)
+        policy = gw.EpsilonGreedyPolicy(grid, args.epsilon)
+        process = Learning(grid, start, policy, args.alpha, args.gamma)
+
+        for (episode, _, reward) in process:
+            if episode > args.episodes:
+                break
+            result = dict(zip(keys, (order, experiment, episode, reward)))
+            outgoing.put(result)
+        outgoing.put(None)
+
+def do(args):
+    incoming = mp.Queue()
+    outgoing = mp.Queue()
+
+    with mp.Pool(args.workers, func, (outgoing, incoming, args)):
+        experiments = {
+            'SARSA': gw.Sarsa,
+            'Q-learning': gw.QLearning,
+        }
+
+        jobs = 0
+        for i in experiments.items():
+            for j in range(args.repeat):
+                outgoing.put((j, *i))
+                jobs += 1
+
+        while jobs:
+            result = incoming.get()
+            if result is None:
+                jobs -= 1
+            else:
+                yield result
+
 arguments = ArgumentParser()
 arguments.add_argument('--alpha', type=float, default=0.1)
 arguments.add_argument('--gamma', type=float, default=1)
 arguments.add_argument('--epsilon', type=float, default=0.1)
-arguments.add_argument('--episodes', type=int, default=8000)
-arguments.add_argument('--rows', type=int, default=4)
-arguments.add_argument('--columns', type=int, default=12)
+arguments.add_argument('--episodes', type=int, default=500)
+arguments.add_argument('--repeat', type=int, default=1)
+arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
-start = gw.State(3, 0)
-goal = gw.State(3, args.columns - 1)
+df = pd.DataFrame.from_dict(do(args))
+logging.info('plotting {}'.format(len(df)))
+print(df.head())
 
-grid = Cliff(args.rows, args.columns, goal, start)
-Q = gw.EpsilonGreedyPolicy(grid, args.epsilon)
+df = df.groupby(['order', 'episode']).sum().reset_index()
 
-fieldnames = [ 'episodes', 'steps' ]
-writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
-writer.writeheader()
-
-for episode in range(args.episodes):
-    logging.info(episode)
-
-    steps = 0
-    state = start
-
-    while state != goal:
-        action = Q.select(state)        
-        (state_, reward) = grid.navigate(state, action)
-
-        Q[now] += args.alpha * (reward + args.gamma * Q.amax(state_) - Q[now])
-        logging.debug("s: {}, a: {}, r: {}, s': {}, Q: {}"
-                      .format(state, action, reward, state_, Q[now]))
-
-        state = state_
-        steps += 1
-
-    writer.writerow(dict(zip(fieldnames, (episode, steps))))
+sns.lineplot(x='episode',
+             y='reward',
+             hue='experiment',
+             data=df)
+plt.grid(True)
+plt.savefig('figure-6.14.png')
