fe01a9b72e78169620770be0720ca4f630b31286
Jerome White <jerome@thewex.net>
2019-01-30 12:38:39 +0530

    Keep states as a list instead of a dictionary
    Keeping them as a dictionary was necessary in Chapter 6 since the
    figure needed to have associated names. That makes reasoning a bit
    tougher than it needs to be in this case.

--- chapters/07/walk.py
+++ chapters/07/walk.py
@@ -1,5 +1,4 @@
 import random
-import operator as op
 import itertools as it
 import functools as ft
 import collections as cl
@@ -12,47 +11,30 @@ Transition = cl.namedtuple('Transition', 'state, action, reward')
 def power(base, exponent):
     return base ** exponent
 
-def walk(states, pos=None, terminal=np.inf):
-    assert(not any([ f(terminal) in states for f in (op.neg, op.pos) ]))
+def walk(states, initial=0):
+    state = states // 2
 
-    n = len(states)
-
-    if pos is None:
-        pos = n // 2
-
-    while 0 <= pos < n:
+    while True:
         action = random.choice((-1, 1))
-        pos_ = pos + action
-        if pos_ < n:
-            reward = 0
-        elif pos_ == n:
-            reward = 1
+        state_ = state + action
+
+        if 0 < state_ < states:
+            yield Transition(state, action, 0)
+            state = state_
         else:
-            reward = -1
-
-        yield Transition(states[pos], action, reward)
-
-        pos = pos_
-
-    yield Transition(np.sign(pos) * terminal, 0, np.nan)
+            reward = 1 if state_ else -1
+            return Transition(state, action, reward)
 
 class Model:
-    def __init__(self, states, episodes, n, alpha):
+    def __init__(self, states, episodes, alpha):
         self.episodes = episodes
         self.alpha = alpha
 
         self.step = 0
-        self.states = []
-        self.V = cl.defaultdict(float)
-
-        for i in range(states):
-            s = chr(ord('A') + i)
-            self.states.append(s)
-            self.V[s] = 0.5
+        self.V = np.zeros(states + 2)
 
     def __iter__(self):
         assert(self.episodes >= 0)
-
         return self
 
     def __next__(self):
@@ -62,15 +44,16 @@ class Model:
             self.update()
         self.step += 1
 
-        return { x: self.V[x] for x in self.states }
+        return self.V
 
     def update(self):
         raise NotImplementedError()
 
-class NStep(Model):
-    def __init__(self, states, episodes, alpha, n=None):
+class TemporalDifference(Model):
+    def __init__(self, states, episodes, alpha, gamma, n=None):
         super().__init__(states, episodes, alpha)
 
+        self.gamma = gamma
         self.n = n
 
     def R(self, window, time, state):
@@ -83,14 +66,13 @@ class NStep(Model):
 
     def delta(self, window):
         for (i, s) in enumerate(window):
-            reward = sum(self.R(window, i, s))
+            reward = sum(self.R(window, i, s.state))
             yield self.alpha * (reward - self.V[s.state])
 
     def update(self):
-        reward = 0
         window = cl.deque(maxlen=self.n)
 
-        for state in walk(self.states):
+        for trans in walk(len(self.V)):
             if not self.n or window.maxlen == self.n:
-                self.V[state] += sum(self.delta(window))
-            window.append(state)
+                self.V[trans.state] += sum(self.delta(window))
+            window.append(trans)
