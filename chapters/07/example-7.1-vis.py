cb9c3a8b0437aeea3c5993df778ebb04a32bb557
Jerome White <jerome@thewex.net>
2019-02-04 18:34:24 +0530

    Visualization

---
+++ chapters/07/example-7.1-vis.py
@@ -0,0 +1,22 @@
+import sys
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = (pd
+      .read_csv(sys.stdin)
+      .sort_values(by=[
+          'online',
+          'alpha',
+          'steps',
+      ])
+
+for (i, data) in df.groupby('online'):
+    plt.clf()
+    sns.lineplot(x='alpha',
+                 y='rmse',
+                 hue='steps',
+                 data=data,
+                 sort=False)
+    plt.savefig(str(i) + '.png')
