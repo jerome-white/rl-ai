d2e0795787ec3aaddfe4f4d4e9a411a7586fe24d
Jerome White <jerome@thewex.net>
2019-02-15 03:27:44 +0530

    Use complete return on truncated episodes.
    Toward the end of the episode, the window will not be full. In these
    cases, consider the true last reward instead of the final state
    value. From the book (pg. 165):
    
    > Of course, if the episode ends in less that n steps, then the
    > truncation in an n-step return occurs at the episode's end,
    > resulting in the conventional complete return.

--- chapters/07/walk.py
+++ chapters/07/walk.py
@@ -53,7 +53,7 @@ class TemporalDifference:
     # Corrected n-step truncated return (pg. 165)
     def R(self, window):
         reward = 0
-        last = len(window) - 1
+        last = window.maxlen - 1
 
         for (i, t) in enumerate(window):
             r = t.reward if i < last else self.V[t.state]
